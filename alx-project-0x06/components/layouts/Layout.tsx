27710f5f3507d580782a14f49efff06f8071639a
MarioTawadrous <mario.tawadrous.95@gmail.com>
2025-08-03 22:26:18 +0300

    Created a duplicate and updated it

---
+++ alx-project-0x06/components/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import { LayoutProps } from "@/interface";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow pt-24">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
